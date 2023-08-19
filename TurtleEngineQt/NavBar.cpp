d465beb3a5bc509ca49f06aa695c312794fa5e36
erik.palencik <erik.palencik@exe.sk>
2023-08-19 18:33:34 +0200

    Update

--- TurtleEngineQt/NavBar.cpp
+++ TurtleEngineQt/NavBar.cpp
@@ -2,6 +2,9 @@
 
 
 #include <QtWidgets/QLabel>
+#include <QtWidgets/qboxlayout.h>
+#include <QtWidgets/qpushbutton.h>
+
 
 
 namespace tui {
@@ -9,6 +12,22 @@ namespace tui {
 	NavBar::NavBar(QWidget* parent) : QWidget(parent)
 	{
 		new QLabel("Magic label", this);
+
+		auto layout = new QBoxLayout(QBoxLayout::Direction::LeftToRight, this);
+
+
+		auto btn1 = new QPushButton();
+		btn1->setText("Btn1");
+
+
+		auto btn2 = new QPushButton();
+		btn1->setText("Btn2");
+
+		layout->addWidget(btn1);
+		layout->addWidget(btn2);
+
+
+
 	}
 
 	NavBar::~NavBar()
