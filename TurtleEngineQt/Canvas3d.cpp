ba9067fc2fa3054f392cc899b15bd17358ce8349
erik.palencik <erik.palencik@exe.sk>
2024-01-28 18:39:15 +0100

    Update

---
+++ TurtleEngineQt/Canvas3d.cpp
@@ -0,0 +1,14 @@
+#include "Canvas3d.h"
+
+
+
+
+Canvas3d::Canvas3d(QWidget* parent) : QWidget(parent)
+{
+
+}
+
+
+Canvas3d::~Canvas3d()
+{
+}
---
+++ TurtleEngineQt/Canvas3d.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <qwidget.h>
+
+
+class Canvas3d : public QWidget
+{
+	Q_OBJECT
+
+public:
+	Canvas3d(QWidget* parent = nullptr);
+	~Canvas3d();
+
+
+};
+
